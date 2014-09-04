6aa94992f6f61f14f34a9fe20c5bae9870522b23
Kev Zettler <kevzettler@gmail.com>
2014-09-04 14:51:29 -0700

    adding websockets

--- ExcelAddIn1/ThisAddIn.cs
+++ ExcelAddIn1/ThisAddIn.cs
@@ -6,6 +6,7 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using WebSocketSharp;
 
 namespace ExcelAddIn1
 {
@@ -14,6 +15,17 @@ namespace ExcelAddIn1
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             //lets start this thing
+            using (var ws = new WebSocket("ws://dragonsnest.far/Laputa"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Excel.Worksheet activeWorksheet = ((Excel.Worksheet)Application.ActiveSheet);
+                    Excel.Range firstRow = activeWorksheet.get_Range("A1");
+                    firstRow.EntireRow.Insert(Excel.XlInsertShiftDirection.xlShiftDown);
+                    Excel.Range newFirstRow = activeWorksheet.get_Range("A1");
+                    newFirstRow.Value2 = "This text was added by using code" + e.data;
+
+                ws.Connect();
+            }
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
